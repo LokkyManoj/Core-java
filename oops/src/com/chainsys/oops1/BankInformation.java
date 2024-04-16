05ef485f563c52abfe7b82f87ac1b8a8467bdacb
mano3578 <mano3578@CSL0693.global.chainsys.com>
2024-04-16 17:52:32 +0530

    Added new(method overloading)

---
+++ oops/src/com/chainsys/oops1/BankInformation.java
@@ -0,0 +1,60 @@
+package com.chainsys.oops1;
+
+public class BankInformation {
+	String userName;
+	String accountNo;
+	String bankLocation;
+	String accountType;
+
+	double amount;
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getAccountNo() {
+		return accountNo;
+	}
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+	public String getBankLocation() {
+		return bankLocation;
+	}
+	public void setBankLocation(String bankLocation) {
+		this.bankLocation = bankLocation;
+	}
+	public String getAccountType() {
+		return accountType;
+	}
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+	
+	public BankInformation(String userName, String accountNo, String bankLocation, String accountType,
+			int aadharNumber) {
+		super();
+		this.userName = userName;
+		this.accountNo = accountNo;
+		this.bankLocation = bankLocation;
+		this.accountType = accountType;
+		
+	}
+	public BankInformation() {
+		
+	}
+	@Override
+	public String toString() {
+		return "BankInformation [userName=" + userName + ", accountNo=" + accountNo + ", bankLocation=" + bankLocation
+				+ ", accountType=" + accountType + "]";
+	}
+
+
+}
---
+++ oops/src/com/chainsys/oops1/BankMain.java
@@ -0,0 +1,83 @@
+package com.chainsys.oops1;
+
+import java.util.Scanner;
+
+public class BankMain {
+	/*String userName;
+	String accountNo;
+	String bankLocation;
+	String accountType;*/
+	
+	double amount;
+public void getUserDetails(BankInformation input) {
+	Validation validate=new Validation();
+	
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter a UserName :");
+	input.setUserName(sc.next());
+	if(!validate.numberchecker(input.userName)) {
+		return;
+	}
+	
+	
+	
+//	if(!validate.isSpecialChar(userName)) {
+//		return;
+//	}
+	
+	System.out.println("Enter a Account Number:");
+	input.setAccountNo( sc.next());
+//	if(validate.isSpecialChar(accountNo)) {
+//		return;
+//	}
+	
+	System.out.println("Enter your Bank Location:");
+	input.setBankLocation(sc.next());
+//	if(!validate.isSpecialChar(bankLocation)) {
+//		return;
+//	}
+
+	System.out.println("Enter your Account Type:");
+	input.setAccountType(sc.next());
+	
+	
+	
+	
+	System.out.println("Enter your Amount:");
+	input.setAmount(sc.nextInt());
+	if(!validate.inputchecker1(amount)) {
+		return;
+	}
+	
+}
+public  void printUserDetails(BankInformation input) {
+	System.out.println(input);
+
+	System.out.println("UserName:"+input.getUserName());
+	System.out.println("AccountNumber:"+input.getAccountNo());
+	System.out.println("Bank Location:"+input.getBankLocation());
+	System.out.println("Account Type:"+input.getAccountType());
+
+	System.out.println("Amount:"+input.getAmount());
+}
+public void deposit(double amount) {
+	 int totalAmount=20000;
+	double total=amount+totalAmount;
+	System.out.println("Total amount in your account is:"+total);
+}
+public void deposit(double amount, String userName) {
+	 int totalAmount=20000;
+	double total=amount+totalAmount;
+	System.out.println("Total amount in your account is:"+total +" depositer Name :" +userName);
+}
+	public static void main(String[] args) {
+BankMain bm=new BankMain();
+BankInformation input=new BankInformation();
+
+bm.getUserDetails(input);
+bm.printUserDetails(input);
+bm.deposit(input.getAmount());
+bm.deposit(input.getAmount(),input.getUserName());
+	}
+
+}
